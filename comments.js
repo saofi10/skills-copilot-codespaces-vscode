89a12911a1ceccfd328bce160648bf1bb386a2e7
saofi10 <129125993+saofi10@users.noreply.github.com>
2023-07-20 02:53:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,15 @@
+// Create web serverapplication
+// 1. Create a web server application that can respond to requests for /hello and return some HTML, such as "Hello World".
+// 2. Add a route for /hello/:name, and return some HTML such as "Hello " + name.
+// 3. Add a route for /opentable/:city, and return a list of the top 10 restaurants in that city in HTML format, with the name, address, and phone number of each restaurant.
+// 4. Add a route for /opentable/:city.json, and return the same information in JSON format.
+// 5. Add a route for /opentable/:city.xml, and return the same information in XML format.
+// 6. Add a route for /opentable/:city/:page, and return the same information in HTML format, but for the specified page number instead of the first page.
+// 7. Add a route for /opentable/:city/:page.json, and return the same information in JSON format, but for the specified page number instead of the first page.
+// 8. Add a route for /opentable/:city/:page.xml, and return the same information in XML format, but for the specified page number instead of the first page.
+// 9. Add a route for /opentable/:city/:page/:pageSize, and return the same information in HTML format, but for the specified page size instead of the default page size of 10.
+// 10. Add a route for /opentable/:city/:page/:pageSize.json, and return the same information in JSON format, but for the specified page size instead of the default page size of 10.
+// 11. Add a route for /opentable/:city/:page/:pageSize.xml, and return the same information in XML format, but for the specified page size instead of the default page size of 10.
+// 12. Add a route for /opentable/:city/:page/:pageSize/:sort, and return the same information in HTML format, but sorted by the specified sort column instead of the default sort column of "name".
+// 13. Add a route for /opentable/:city/:page/:pageSize/:sort.json, and return the same information in JSON format, but sorted by the specified sort column instead of the default sort column of "name".
+// 14. Add
